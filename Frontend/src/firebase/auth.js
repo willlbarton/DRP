0db8cd0c06a59d6f61559063f07821c28bcb7b2e
will <williamjb2004@gmail.com>
2024-06-05 20:16:41 +0100

    feat: Add auth for logins

---
+++ Frontend/src/firebase/auth.js
@@ -0,0 +1,21 @@
+import { auth } from "./firebase";
+
+import { createUserWithEmailAndPassword, GoogleAuthProvider, signInWithEmailAndPassword, signInWithPopup } from "firebase/auth";
+
+export const doCreateUserWithEmailAndPassword = async (email, password) => {
+    return createUserWithEmailAndPassword(auth, email, password);
+};
+
+export const doSignInWithEmailAndPassword = (email, password) => {
+    return signInWithEmailAndPassword(auth, mail, password);
+};
+
+export const doSignInWithGoogle = async () => {
+    const provider = new GoogleAuthProvider();
+    const result = await signInWithPopup(auth, provider);
+    return result;
+};
+
+export const doSignOut = () => {
+    return auth.signOut();
+}
