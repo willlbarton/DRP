87014fc2563c8078a9c0901648be4f8931bc9328
will <williamjb2004@gmail.com>
2024-06-06 00:54:39 +0100

    fix: remove unused files

--- src/main/java/ic/doc/web/Page.java
+++
@@ -1,9 +0,0 @@
-package ic.doc.web;
-
-import javax.servlet.http.HttpServletResponse;
-import java.io.IOException;
-
-public interface Page {
-
-    void writeTo(HttpServletResponse resp) throws IOException;
-}
