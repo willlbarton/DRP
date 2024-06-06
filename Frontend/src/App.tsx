5e337f013ba78882883e5611b146403a4ef2ad29
will <williamjb2004@gmail.com>
2024-06-06 17:20:21 +0100

    feat: add home page

--- Frontend/src/App.tsx
+++ Frontend/src/App.tsx
@@ -3,14 +3,18 @@ import { BrowserRouter as Router, Route, Routes, Link } from "react-router-dom";
 import HammersmithForm from "./pages/HammersmithForm";
 import Register from "./pages/Register";
 import Login from "./pages/Login";
+import Home from "./pages/Home";
 
 const App = () => {
   return (
     <>
       <nav>
         <ul>
+        <li>
+            <Link to="/">Home</Link>
+          </li>
           <li>
-            <Link to="/">Login</Link>
+            <Link to="/login">Login</Link>
           </li>
           <li>
             <Link to="/register">Register</Link>
@@ -21,7 +25,8 @@ const App = () => {
         </ul>
       </nav>
       <Routes>
-        <Route path="/" element={<Login />} />
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Register />} />
         <Route path="/form" element={<HammersmithForm />} />
       </Routes>
