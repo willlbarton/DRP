b7e819e3b13ef84012d51c65ca0f9958fae46637
will <williamjb2004@gmail.com>
2024-06-06 18:53:00 +0100

    feat: only show one of login/sign out

--- Frontend/src/layouts/MainLayout.tsx
+++ Frontend/src/layouts/MainLayout.tsx
@@ -4,8 +4,8 @@ import { doSignOut } from '@/firebase/auth';
 import { useAuth } from '@/contexts/authContexts';
 
 const MainLayout = () => {
-  const navigate = useNavigate()
-  const { currentUser } = useAuth()
+  const navigate = useNavigate();
+  const { currentUser } = useAuth();
 
   const handleSignOut = async () => {
     await doSignOut();
@@ -13,52 +13,58 @@ const MainLayout = () => {
   };
 
   const goToForm = () => {
-    if(currentUser) {
-        navigate("/form")
+    if (currentUser) {
+      navigate("/form");
     }
-  }
+  };
 
   const goToLogin = () => {
-    if(!currentUser) {
-        navigate("/login")
+    if (!currentUser) {
+      navigate("/login");
     }
-  }
+  };
 
   return (
     <div className="min-h-screen flex flex-col">
-      <header className="bg-black text-white fixed top-0 left-0 right-0 z-50">
-        <nav className="container mx-auto p-4">
-          <ul className="flex space-x-6">
+      <header className="bg-black text-white fixed top-0 left-0 right-0 z-50 h-20">
+        <nav className="container mx-auto h-full flex items-center justify-between">
+          <ul className="flex w-full justify-evenly text-lg">
             <li>
               <Link to="/" className="hover:text-gray-300">
                 Home
               </Link>
             </li>
             <li>
-            <button
+              <button
                 onClick={goToForm}
-                className="hover:text-gray-300 focus:outline-none">
+                className="hover:text-gray-300 focus:outline-none"
+              >
                 Form
               </button>
             </li>
-            <li>
-            <button
-                onClick={goToLogin}
-                className="hover:text-gray-300 focus:outline-none">
-                Login
-              </button>
-            </li>
-            <li>
-              <button
-                onClick={handleSignOut}
-                className="hover:text-gray-300 focus:outline-none">
-                Sign out
-              </button>
-            </li>
+            {currentUser ? (
+              <li>
+                <button
+                  onClick={handleSignOut}
+                  className="hover:text-gray-300 focus:outline-none"
+                >
+                  Sign out
+                </button>
+              </li>
+            ) : (
+              <li>
+                <button
+                  onClick={goToLogin}
+                  className="hover:text-gray-300 focus:outline-none"
+                >
+                  Login
+                </button>
+              </li>
+            )}
           </ul>
         </nav>
       </header>
-      <main className="flex-grow container mx-auto p-4 pt-16 flex justify-center items-center">
+      <main className="flex-grow container mx-auto p-4 pt-20 flex justify-center items-center">
         <Outlet />
       </main>
     </div>
