b508db1716678533007aa4d51d5d5d1745558cb1
will <williamjb2004@gmail.com>
2024-06-07 11:19:26 +0100

    feat: store form in app

--- Frontend/src/pages/FormViewer.tsx
+++ Frontend/src/pages/FormViewer.tsx
@@ -1,8 +1,31 @@
-import React from 'react'
+import React, { useState } from 'react';
 import { PDFDocument } from 'pdf-lib';
 import { Card, CardTitle, CardContent } from '@/components/ui/card';
 
 const FormViewer = () => {
+  const [pdfBlob, setPdfBlob] = useState(null);
+
+  const fillPdf = async () => {
+    const existingPdfBytes = await fetch('/path/to/application_for_council_tax_student_discount.pdf').then(res => res.arrayBuffer());
+    const pdfDoc = await PDFDocument.load(existingPdfBytes);
+    const form = pdfDoc.getForm();
+  }
+
+  const listFormFields = async () => {
+    const existingPdfBytes = await fetch('form.pdf').then(res => res.arrayBuffer());
+    const pdfDoc = await PDFDocument.load(existingPdfBytes);
+    const form = pdfDoc.getForm();
+    const fields = form.getFields();
+  
+    fields.forEach(field => {
+      const type = field.constructor.name;
+      const name = field.getName();
+      console.log(`${type}: ${name}`);
+    });
+  };
+  
+  listFormFields();
+
   return (
     <div className="flex flex-col w-screen">
       <Card className="p-8 self-center w-[90%] max-w-[800px]">
