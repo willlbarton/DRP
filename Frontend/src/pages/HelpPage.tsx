20dfeaa46ccf937354eb435eabee124e7def3547
Reuben <rc922@ic.ac.uk>
2024-06-13 16:13:44 +0100

    feat: can select a time.

--- Frontend/src/pages/HelpPage.tsx
+++ Frontend/src/pages/HelpPage.tsx
@@ -1,9 +1,48 @@
-import React from 'react'
+import { Button } from '@/components/ui/button';
+import React, { useState } from 'react'
 
-const HelpPage = () => {
+const TimeSelector = () => {
+  const [selectedTime, setSelectedTime] = useState("");
+  var updateTime = (event: any) => {
+    const t = event.target.getAttribute("data-time");
+    console.log("PRESSED!" + t)
+    setSelectedTime(t);
+  }
+  // assume 9am to 5pm, 15-minute intervals.
+  const times = [9,10,11,12,13,14,15,16,17].map((hour) =>
+    <div>
+      <Button variant="ghost" className="ml-2" onClick={updateTime} data-time={hour+":00"}>
+        {hour.toString()+":00"}
+      </Button>
+      <Button variant="ghost" className="ml-2" onClick={updateTime} data-time={hour+":15"}>
+        {hour.toString()+":15"}
+      </Button>
+      <Button variant="ghost" className="ml-2" onClick={updateTime} data-time={hour+":30"}>
+        {hour.toString()+":30"}
+      </Button>
+      <Button variant="ghost" className="ml-2" onClick={updateTime} data-time={hour+":45"}>
+        {hour.toString()+":45"}
+      </Button>
+    </div> )
   return (
-    <div>HelpPage</div>
+    <div>
+      Selected time: {selectedTime}
+      <br></br>
+      {times}
+    </div>
   )
 }
+const HelpPage = () => {
+
+  return (
+    <>
+    <div>HelpPage</div>
+    <div>
+      <TimeSelector />
+    </div>
+    </>
+  )
+}
+
 
 export default HelpPage
