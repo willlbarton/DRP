ff0b24ef0ceae9aa4f3336c9a594a647fe4c21c6
Reuben <rc922@ic.ac.uk>
2024-06-13 20:34:09 +0100

    feat: server sends initial availabilities.

--- Frontend/api/index.tsx
+++ Frontend/api/index.tsx
@@ -21,6 +21,27 @@ const io = new Server(httpServer, {
   credentials: true,
  },
 });
+
+const times = [9,10,11,12,13,14,15,16,17].flatMap((hour) => [
+    { time: `${hour}:00`, display: `${hour}:00` },
+    { time: `${hour}:15`, display: `${hour}:15` },
+    { time: `${hour}:30`, display: `${hour}:30` },
+    { time: `${hour}:45`, display: `${hour}:45` }
+]);
+// for now, do not load availabilities from database.
+let availability = new Map(times.map((time) => [time, true]));
+// handle new connections
+let connections : any[] = [];
+io.on('connect', (socket) => {
+    connections.push(socket);
+    console.log("New client connection to server.");
+    socket.emit("servedAvailability", availability);
+
+    // Handle all signals received from client below (e.g. socket.on(...))
+
+    // probably ought to handle disconnects too.
+});
+
 httpServer.listen(PORT, () => {
  console.log(`Server is listening on port ${PORT}`);
 });
