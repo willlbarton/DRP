5530faba20901d462302d57194b2012c2cb2e8f0
will <williamjb2004@gmail.com>
2024-06-20 06:07:13 +0100

    feat: add calendar for booking

--- Frontend/src/pages/HelpPage.tsx
+++ Frontend/src/pages/HelpPage.tsx
@@ -2,25 +2,31 @@ import { Button } from '@/components/ui/button';
 import React, { useEffect, useState } from 'react';
 import { db } from '@/firebase/firebase';
 import { doc, setDoc, onSnapshot, collection } from "firebase/firestore";
+import { format, set, addHours, addMinutes, isAfter } from "date-fns"
+import { Calendar as CalendarIcon } from "lucide-react"
+import { cn } from "@/lib/utils"
 import { Calendar } from "@/components/ui/calendar"
 import {
   Popover,
   PopoverContent,
   PopoverTrigger,
 } from "@/components/ui/popover"
+import { useAuth } from '@/contexts/authContexts';
+import { fill } from 'pdf-lib';
 
 
 const HelpPage = () => {
   const [selectedTime, setSelectedTime] = useState("");
   const [availability, setAvailability] = useState(new Map<string, boolean>());
-  const [date, setDate] = useState(new Date());
+  const [date, setDate] = React.useState<Date>()
+  const { currentUser } = useAuth();
 
   useEffect(() => {
     const unsubscribe = onSnapshot(collection(db, "bookings"), (snapshot) => {
       const newAvailability = new Map<string, boolean>();
       snapshot.forEach((doc) => {
         const data = doc.data();
-        newAvailability.set(doc.id, data.status !== "booked");
+        newAvailability.set(doc.id, (data.status !== "booked") && true);
       });
       setAvailability(newAvailability);
     });
@@ -34,17 +40,29 @@ const HelpPage = () => {
   };
 
   const isAvailable = (time: string) => {
-    if (!availability.has(time)) {
-      return true;
+    const [hours, minutes] = time.split(":").map(Number);
+    console.log("hours: ", hours, "minutes: ", minutes)
+    var d = date ? date : new Date();
+    console.log(d)
+    d = addHours(d, hours)
+    d = addMinutes(d, minutes)
+    console.log(d)
+    if (isAfter(d, new Date())) {
+      if (!availability.has(time)) {
+        return true;
+      }
+      return availability.get(time);
     }
-    return availability.get(time);
+    return false
   };
 
   const bookTime = async () => {
     if (availability.has(selectedTime) && isAvailable(selectedTime)) {
       try {
-        const docRef = doc(db, 'bookings', selectedTime);
+        const dat = format(date ? date : new Date(), "yyyy-MM-dd");
+        const docRef = doc(db, 'bookings', dat, selectedTime);
         await setDoc(docRef, { status: "booked" });
+        await setDoc(docRef, { user: currentUser?.uid });
         setAvailability(prevAvailability => {
           const newAvailability = new Map(prevAvailability);
           newAvailability.set(selectedTime, false);
@@ -57,6 +75,8 @@ const HelpPage = () => {
     }
   };
 
+  
+
   const times = [9, 10, 11, 12, 13, 14, 15, 16, 17].flatMap((hour) => [
     { time: `${hour}:00`, display: `${hour}:00` },
     { time: `${hour}:15`, display: `${hour}:15` },
@@ -64,29 +84,67 @@ const HelpPage = () => {
     { time: `${hour}:45`, display: `${hour}:45` }
   ]);
 
+  const filltoday = () => {
+    const today = format(new Date(), "yyyy-MM-dd");
+    console.log("yassss")
+    times.forEach(async (time) => {
+      const docRef = doc(db, 'bookings', today, 'slots', time.time);
+      await setDoc(docRef, { status: "available" });
+      await setDoc(docRef, { user: currentUser?.uid });
+    });
+  }
+
+  filltoday();
+
   return (
     <div className="flex justify-center">
       <div className="flex flex-col items-center">
-        <p className="text-lg font-bold mb-4">Selected time: {selectedTime}</p>
-        {/* <Calendar /> */}
-        <div className="grid grid-cols-4 gap-4">
-          {times.map((slot, index) => (
+        <Popover>
+          <PopoverTrigger asChild>
             <Button
-              key={index}
-              variant="ghost"
-              onClick={updateTime}
-              data-time={slot.time}
-              className="p-2 border border-none text-center w-full"
-              style={{ color: isAvailable(slot.time) ? "green" : "red" }}
-              disabled={!isAvailable(slot.time)}
+              variant={"outline"}
+              className={cn(
+                "w-[280px] justify-start text-left font-normal",
+                !date && "text-muted-foreground"
+              )}
             >
-              {slot.display}
+              <CalendarIcon className="mr-2 h-4 w-4" />
+              {date ? format(date, "PPP") : <span>Pick a date</span>}
             </Button>
-          ))}
+          </PopoverTrigger>
+          <PopoverContent className="w-auto p-0">
+            <Calendar
+              mode="single"
+              selected={date}
+              onSelect={setDate}
+              initialFocus
+              disabled={{ before: new Date() }}
+            />
+          </PopoverContent>
+        </Popover>
+        {date &&
+        <div className='flex flex-col items-center'>
+          <p className="text-lg font-bold mb-4">Selected time: {selectedTime}</p>
+          <div className="grid grid-cols-4 gap-4">
+            {times.map((slot, index) => (
+              <Button
+                key={index}
+                variant="ghost"
+                onClick={updateTime}
+                data-time={slot.time}
+                className="p-2 border border-none text-center w-full"
+                style={{ color: isAvailable(slot.time) ? "green" : "red" }}
+                disabled={!isAvailable(slot.time)}
+              >
+                {slot.display}
+              </Button>
+            ))}
+          </div>
+          <Button key="submit" variant="ghost" onClick={bookTime}>
+            Book!
+          </Button>
         </div>
-        <Button key="submit" variant="ghost" onClick={bookTime}>
-          Book!
-        </Button>
+        }
       </div>
     </div>
   );
